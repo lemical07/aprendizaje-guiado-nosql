210f80b36cc1ff44797054402cf669e800c7ad1b
Anderson-Oloroso <henrikog4@gmail.com>
2026-09-01 10:45:08 -0600

    :sparkles: feat(json): Add script of importing json data

---
+++ script/dml/import_categories_json.sql
@@ -0,0 +1,17 @@
+CREATE TABLE temporal_json(data JSONB);
+
+\copy temporal_json(data) FROM PROGRAM 'tr -d "\r\n" < /home/camper/Música/HNK/psql/no-sql/categorias.json'
+
+INSERT INTO categorias (codigo, nombre, descripcion)
+SELECT
+    e->>'codigo' AS codigo,
+    e->>'nombre' AS nombre,
+    e->>'descripcion' AS descripcion
+FROM temporal_json AS t 
+CROSS JOIN LATERAL jsonb_array_elements(t.data) AS e;
+
+SELECT categoria_id, codigo, nombre
+FROM categorias
+ORDER BY categoria_id;
+
+DROP TABLE temporal_json;
